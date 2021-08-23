df647ae2297eb43b293b6d8cff5709a4401bb3b2
Unitimu <uiuiunitimu@gmail.com>
2021-08-22 21:03:33 -0300

    pensei em um uso para o datetime

--- LojaDeBikes.py
+++ LojaDeBikes.py
@@ -19,9 +19,9 @@ class Cliente(object):
             if stempo not in 'horasdiassemanas':
                 raise NameError
 
-            if stempo in 'horas':
-                print('joquei, GUIGUI')
-                print('Pedro, GUILHERME ')
+            # if stempo in 'horas':
+            #     print('joquei, GUIGUI')
+            #     print('Pedro, GUILHERME ')
 
 
 
@@ -42,8 +42,26 @@ class Cliente(object):
 
 class LojadeBikes(object):
     def __init__(self,nbicicletas):
+        # self.bikes será usado para representar quanto tempo falta para cada bike ficar disponível,
+        # então qnd um cliente alugar 3 bikes por 5 horas , 3 bikes com o value '0' terão um novo valor de 5 horas ,
+        # que será atualizado a cada hora(?)
         self.nbikes = nbicicletas
         self.money = 0
+        self.bikes = {
+            'bike1' : 0,
+            'bike2' : 0,
+            'bike3' : 0,
+            'bike4' : 0,
+            'bike5' : 0,
+            'bike6' : 0,
+            'bike7' : 0,
+            'bike8' : 0,
+            'bike9' : 0,
+            'bike10' : 0,
+            'bike11' : 0,
+
+
+        }
 
 eu = Cliente()
 lojinha = LojadeBikes(15)
