480922e22f191e9daa8f18d75fbdb8f3148702fe
“Pedro <pedromoraes8@hotmail.com>
2021-08-25 20:15:24 -0300

    crie uma classe loja

--- LojaDeBikes.py
+++ LojaDeBikes.py
@@ -73,3 +73,102 @@ eu.verificarBicicletas(lojinha)
 
 print('bananas')
 
+# PEDRO AQUI ---------------------------------------
+
+
+# CLASSE CLIENTE - DIRECIONAMENTO
+#Ver as bicicletas disponíveis na Loja;
+#Alugar bicicletas por hora (R$5/hora);
+#Alugar bicicletas por dia (R$25/dia);
+#Alugar bicicletas por semana (R$100/semana)
+#Aluguel para família, uma promoção que pode incluir de 3 a 5 empréstimos (de qualquer tipo) com 30% de desconto no valor total.
+
+class Cliente(object):
+    pass
+
+
+# CLASSE LOJA - DIRECIONAMENTO
+#Calcular a conta quando o cliente decidir devolver a bicicleta;
+#Mostrar o estoque de bicicletas - DONE 
+#Receber pedidos de aluguéis por hora, diários ou semanais validando a possibilidade com o estoque.
+
+class Loja(object):
+    def __init__(self, estoqueBikes, caixa):
+        self.estoqueBikes = estoqueBikes
+        self.caixa = caixa
+        self.precoHora = 10
+        self.precoDia = 180
+        self.precoSemana = 900
+
+    #Mostrar o estoque de bicicletas
+
+    def mostraEstoque(self):
+        # fazer um print do estoque de bikes
+        try:
+            if self.estoqueBikes > 1:
+                print(f"\nEstoque de Bicicletas - {self.estoqueBikes} bikes.")
+            
+            elif self.estoqueBikes == 1:
+                print(f"\nEstoque de Bicicletas - {self.estoqueBikes} bike.")
+
+            else:
+                print(f"\nEstoque de Bicicletas - Vazio no momento.")
+                return
+
+        except:
+            # raise na possibilidade do estoque ser negativo
+            # levantar erros possíveis - ValueError e etc..
+            pass
+
+    #Receber pedidos de aluguéis por hora, diários ou semanais validando a possibilidade com o estoque.
+    #Calcular a conta quando o cliente decidir devolver a bicicleta; 
+    # SUPER FUNCAO KKK
+
+    def valorTempoAluguelBikes(self, numeroBikes, tempoAluguel):
+        while numeroBikes > self.estoqueBikes:
+            print("\nPedido recebido - Total de bikes não disponível no momento || Total bikes disponíveis no estoque: {self.estoqueBikes}.")
+            validaAluguel = input("Você ainda quer alugar alguma bike? Resp: Sim ou Não")
+            if validaAluguel.lower().strip() == "sim":
+                numeroBikes = int(input(f"\nQuantas bikes você quer alugar? "))
+        
+            else:
+                break
+        # se o user digitar algo diferente de sim, ele sai da funcao e já era, caso nao, ele coloca um numero valido e segue
+        # caso já nao tenha colocado um numeroBikes valido antes
+
+        self.estoqueBikes -= numeroBikes
+
+        # usar datetime aqui, apenas um exemplo de como seria
+        try:
+            if tempoAluguel == 'hora':
+                valorHora = 'hora' * self.precoHora
+                valorConta = numeroBikes * valorHora
+                print("Pedido recebido - {numeroBikes} durante {tempoAluguel} hora/s, total de R$ {valorConta} reais, pedido confirmado || Total bikes disponíveis no estoque: {self.estoqueBikes}.")
+                numeroBikesRecebidos = numeroBikes
+                return True
+
+            if tempoAluguel == 'dia':
+                valorDia = 'dia' * self.precoDia
+                valorConta = numeroBikes * valorDia
+                print("Pedido recebido - {numeroBikes} durante {tempoAluguel} dia/s solicitadas, total de R$ {valorConta} reais, pedido confirmado || Total bikes disponíveis no estoque: {self.estoqueBikes}.")
+                numeroBikesRecebidos = numeroBikes
+                return True
+
+            if tempoAluguel == 'semana':
+                valorSemana = 'semana' * self.precoSemana
+                valorConta = numeroBikes * valorSemana
+                print("Pedido recebido - {numeroBikes} durante {tempoAluguel} semana/s solicitadas, total de R$ {valorConta} reais, pedido confirmado || Total bikes disponíveis no estoque: {self.estoqueBikes}.") 
+                numeroBikesRecebidos = numeroBikes
+                return True
+
+
+        except:
+            pass
+    
+
+
+
+
+#Cada empréstimo segue apenas um modelo de cobrança (hora, dia ou semana);
+#O cliente pode decidir livremente quantas bicicletas quer alugar;
+#Os pedidos de aluguéis só podem ser efetuados se houver bicicletas suficientes disponíveis.
