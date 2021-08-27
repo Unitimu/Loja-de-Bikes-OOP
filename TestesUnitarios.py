187a31d1077e57122bbcfae1048444b1f22d7958
GUILAZE <lafont.azevedo@outlook.com>
2021-08-27 20:27:39 -0300

    Primeiros passos

--- TestesUnitarios.py
+++ TestesUnitarios.py
@@ -1,1 +1,10 @@
-print('oi-')
+import unittest
+from LojaDeBikes import Loja, Cliente
+
+class teste(unittest.TestCase):
+    def setUp(self):
+        self.Loja =Loja(3, 10)
+        self.cliente = Cliente()
+
+    
+
