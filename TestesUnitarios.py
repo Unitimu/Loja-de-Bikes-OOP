0bf0953d8e5d7a86d4fb9ef472f65fd8c136a01f
GUILAZE <lafont.azevedo@outlook.com>
2021-08-27 20:52:34 -0300

    função dentro da classe

--- TestesUnitarios.py
+++ TestesUnitarios.py
@@ -3,8 +3,12 @@ from LojaDeBikes import Loja, Cliente
 
 class teste(unittest.TestCase):
     def setUp(self):
-        self.Loja =Loja(3, 10)
+        self.Loja = Loja(3)
         self.cliente = Cliente()
 
-    
+    def testVerificarTempo(self):
+        print("Teste para verificar o tempo")
+        self.assertEqual(self.cliente.verificarTempo("horas"), "horas")
 
+if __name__ == "__main__":
+    unittest.main()
